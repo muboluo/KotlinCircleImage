d8d2c9d500afe74b3afeeb820de34719031192e0
henry <muboluo@126.com>
2018-12-13 09:40:32 +0800

    第一次提交 创建工程

---
+++ app/src/main/java/img/circle/kt/com/kotlincircleimage/MainActivity.kt
@@ -0,0 +1,12 @@
+package img.circle.kt.com.kotlincircleimage
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
