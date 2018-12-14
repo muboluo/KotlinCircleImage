09e8de1fc5a94cb8a1808788dd01a89ffc96237d
henry <muboluo@126.com>
2018-12-14 10:04:41 +0800

    完成  constraint layout 的布局

---
+++ app/src/main/java/img/circle/kt/com/widget/CircleImg.kt
@@ -0,0 +1,16 @@
+package img.circle.kt.com.widget
+
+import android.content.Context
+import android.support.v7.widget.AppCompatImageView
+import android.util.AttributeSet
+
+/**
+ * Created by henry on 2018/12/14.
+ */
+class CircleImg(context: Context, defStyleAttributes: AttributeSet, defAttrStyle: Int = 0) : AppCompatImageView(context,
+
+        defStyleAttributes, defAttrStyle) {
+
+
+
+}
