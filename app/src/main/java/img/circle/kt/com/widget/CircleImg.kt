576cf863df44a9064fbcb981c10421fd4ce44876
henry <muboluo@126.com>
2019-01-02 10:25:43 +0800

    添加部分属性

--- app/src/main/java/img/circle/kt/com/widget/CircleImg.kt
+++ app/src/main/java/img/circle/kt/com/widget/CircleImg.kt
@@ -1,6 +1,7 @@
 package img.circle.kt.com.widget
 
 import android.content.Context
+import android.graphics.Color
 import android.support.v7.widget.AppCompatImageView
 import android.util.AttributeSet
 
@@ -11,6 +12,62 @@ class CircleImg(context: Context, defStyleAttributes: AttributeSet, defAttrStyle
 
         defStyleAttributes, defAttrStyle) {
 
+
+    enum class ShapeType {
+
+        SHAPE_CIRCLE,
+        SHAPE_ROUND
+    }
+
+    private var shape_type: ShapeType = ShapeType.SHAPE_CIRCLE
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    private var border_width: Float = 20f
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    private var border_color: Int = Color.parseColor("#ff9900")
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    private var left_top_radiusX: Float = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    private var left_top_radiusY: Float = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    private var right_top_radiusX: Float = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    private var right_top_radiusY: Float = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    private var right_bottom_radiusX: Float = 0f
+        set(value) {
+
+            field = value
+            invalidate()
+
+        }
 
 
 }
